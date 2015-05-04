063139d4f92ffc2c6f506f885c80c95959848bc5
Chris Brauchli <cbrauchli@gmail.com>
2015-05-04 13:56:27 -0700

    initial commit

---
+++ UberSDK/UberSDK/UberSDK/UberSDK.h
@@ -0,0 +1,19 @@
+//
+//  UberSDK.h
+//  UberSDK
+//
+//  Created by Chris Brauchli on 5/4/15.
+//  Copyright (c) 2015 Uber. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UberSDK.
+FOUNDATION_EXPORT double UberSDKVersionNumber;
+
+//! Project version string for UberSDK.
+FOUNDATION_EXPORT const unsigned char UberSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UberSDK/PublicHeader.h>
+
+
