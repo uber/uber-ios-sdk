a58abe82787e2f756e20c447d466b1a681070611
Christine Kim <chkim@uber.com>
2015-11-20 20:22:50 +0000

    Swift iOS SDK - README
    Reviewers: juanp, goutham, jneuman, texter, go
    
    Reviewed By: go
    
    Subscribers: #external_api
    
    Differential Revision: https://code.uberinternal.com/D254008

--- source/UberRides/UberRides.h
+++ source/UberRides/UberRides.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double UberRidesVersionNumber;
 FOUNDATION_EXPORT const unsigned char UberRidesVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <UberRides/PublicHeader.h>
-
-
