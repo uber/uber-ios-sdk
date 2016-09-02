15853d1ba1d3d8530214f316d9124af3cb14140c
Charlyn Gonda <charlyn@uber.com>
2016-09-02 16:07:44 -0700

    Tutorial Companion Code
    This is to:
    1. Provide an immediately available boilerplate code that already
    includes the Uber iOS SDK to start working on the step by step guides
    right away.
    2. Give an example of a finished product for the step by step guides.

---
+++ examples/Tutorials/TutorialBase/TutorialBase/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TutorialBase
+
+import UIKit
+import UberRides
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
+        // If true, all requests will hit the sandbox, useful for testing
+        Configuration.setSandboxEnabled(true)
+        
+        return true
+    }
+}
+
---
+++ examples/Tutorials/TutorialBase/TutorialBase/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TutorialBase
+
+
+import UIKit
+import UberRides
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // add tutorial code here
+    }
+
+}
+
