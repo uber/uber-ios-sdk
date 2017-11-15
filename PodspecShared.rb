d0a66c79c14e9773adf493fce2dcd77a19b3cad2
Edward Jiang <edjiang@uber.com>
2017-11-15 13:53:53 -0800

    move common podspec info into shared file

---
+++ PodspecShared.rb
@@ -0,0 +1,18 @@
+def configure_podspec(spec, module_name)
+  spec.name         = module_name
+  spec.version      = "0.7.0"
+  spec.summary      = "The Official Uber Rides iOS SDK."
+  spec.description  = <<-DESC
+    This Swift library allows you to integrate Uber into your iOS app. It is designed to make it quick and easy to add a 'Request a Ride' button in your application, seamlessly connecting your users with Uber.
+                   DESC
+  spec.homepage     = "https://github.com/uber/rides-ios-sdk"
+  spec.screenshots  = "https://raw.githubusercontent.com/uber/rides-ios-sdk/master/img/example_app.png"
+  spec.license      = { :type => "MIT", :file => "LICENSE" }
+  spec.authors      = { "Edward Jiang" => "edjiang@uber.com", "Jay Bobzin" => "jbobzin@uber.com", "Ty Smith" => "tys@uber.com" }
+  spec.platform     = :ios, "8.0"
+  spec.source       = { :git => "https://github.com/uber/rides-ios-sdk.git", :tag => 'v' + spec.version.to_s }
+  spec.source_files = ["source/" + module_name + "/**/*.{swift,h,m}"]
+  spec.resource     = "source/" + module_name + "/Resources/**"
+  spec.requires_arc = true
+  spec.ios.deployment_target = '8.0'
+end
--- UberCore.podspec
+++ UberCore.podspec
@@ -1,20 +1,5 @@
+require_relative("PodspecShared")
+
 Pod::Spec.new do |s|
-
-  s.name         = "UberCore"
-  s.version      = "0.7.0"
-  s.summary      = "The Official Uber iOS SDK."
-  s.description  = <<-DESC
-    This Swift library allows you to integrate Uber into your iOS app. It is designed to make it quick and easy to add a 'Request a Ride' button in your application, seamlessly connecting your users with Uber.
-                   DESC
-  s.homepage     = "https://github.com/uber/rides-ios-sdk"
-  s.screenshots  = "https://raw.githubusercontent.com/uber/rides-ios-sdk/master/img/example_app.png"
-  s.license      = { :type => "MIT", :file => "LICENSE" }
-  s.authors      = { "Edward Jiang" => "edjiang@uber.com", "Jay Bobzin" => "jbobzin@uber.com", "Ty Smith" => "tys@uber.com" }
-  s.platform     = :ios, "8.0"
-  s.source       = { :git => "https://github.com/uber/rides-ios-sdk.git", :tag => 'v' + s.version.to_s }
-  s.source_files = ["source/UberCore/**/*.{swift,h,m}"]
-  s.resource     = "source/UberCore/Resources/**"
-  s.requires_arc = true
-  s.ios.deployment_target = '8.0'
-
+  configure_podspec(s, "UberCore")
 end
